66dfc28d22c939fc6c9cf778476e2bae4336b089
Artem Egorov <Artem-Egorov13@mail.ru>
2022-10-05 18:08:57 +0300

    Create programm

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 22000;
+
+         int amount = 20;
+
+          int miles = ticket / amount;
+
+        System.out.println("Начисленные милли" + miles);
+    }
 }
