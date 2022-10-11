82a66cc0939f625665290041e5ded85125bd182d
Artem Egorov <Artem-Egorov13@mail.ru>
2022-10-11 15:31:10 +0300

    fix cod

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
         int ticket = 22000;
 
-         int amount = 20;
+        int amount = 20;
 
-          int miles = ticket / amount;
+        int miles = ticket / amount;
 
         System.out.println("Начисленные милли" + miles);
     }
